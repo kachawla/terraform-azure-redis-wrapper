28dbf2116b9c64e97190c993d363ab725b71feec
karishma-chawla <74574173+karishma-chawla@users.noreply.github.com>
2023-09-12 09:41:33 -0700

    Add context variable

--- main.tf
+++ main.tf
@@ -7,10 +7,13 @@ terraform {
   }
 }
 
+resource "random_uuid" "name" {
+}
+
 module "redis" {
   source  = "kachawla/test/azure"
 
-  redis_cache_name    = var.redis_cache_name
+  redis_cache_name    = "${random_uuid.name.result}-redis"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.context.azure.resourceGroup.name
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,10 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the Azure resource group."
-}
-
 variable "location" {
   type        = string
-  description = "Redis cache deployment region. Can be different from resource group location."
+  description = "Redis cache deployment region."
 }
 
-variable "redis_cache_name" {
-  type        = string
-  description = "Name of the Redis Cache."
+variable "context" {
+  description = "This variable contains Radius recipe context."
+
+  type = any
 }
