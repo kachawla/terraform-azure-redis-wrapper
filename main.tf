02c211fe89f8ccf7d35921f9659b233b9c874924
Karishma Chawla <kachawla@microsoft.com>
2023-09-12 19:30:23 -0700

    Use random_id

--- main.tf
+++ main.tf
@@ -7,13 +7,19 @@ terraform {
   }
 }
 
-resource "random_uuid" "name" {
+resource "random_id" "resource" {
+  keepers = {
+    # Generate a new id for every unique resource id
+    ami_id = var.context.resource.id
+  }
+
+  byte_length = 8
 }
 
 module "redis" {
   source  = "kachawla/test/azure"
 
-  redis_cache_name    = "redis-${random_uuid.name.result}"
+  redis_cache_name    = "redis-${random_id.resource.hex}"
   location            = var.location
   resource_group_name = var.context.azure.resourceGroup.name
 }
